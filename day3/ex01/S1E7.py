53c8dbfcf5ba487bd114cc726296811fe16cd618
Romuald Garcia <rgarcia@z3r3p3.42lyon.fr>
2025-03-07 18:42:31 +0100

    day3 ex00 01

--- day3/ex01/S1E7.py
+++ day3/ex01/S1E7.py
@@ -3,12 +3,12 @@ from S1E9 import Character
 
 class Baratheon(Character):
     """Baratheon Class that inherits from Character"""
-    def __init__(self, first_name, is_alive=True, family_name="Baratheon", eyes="brown", hair="dark"):
+    def __init__(self, first_name, is_alive=True):
         """Baratheon constructor"""
         super().__init__(first_name, is_alive)
-        self.family_name = family_name
-        self.eyes = eyes
-        self.hair = hair
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hair = "dark"
 
     def __str__(self):
         """Baratheon str method"""
@@ -17,21 +17,20 @@ class Baratheon(Character):
     def __repr__(self):
         """Baratheon repr method"""
         return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hair}')"
-    
+
     def die(self):
         """Baraetheon die method"""
         self.is_alive = False
 
 
-
 class Lannister(Character):
     """Lannister Class that inherits from Character"""
-    def __init__(self, first_name, is_alive=True, family_name="Lannister", eyes="blue", hair="light"):
+    def __init__(self, first_name, is_alive=True):
         """Lannister constructor"""
         super().__init__(first_name, is_alive)
-        self.family_name = family_name
-        self.eyes = eyes
-        self.hair = hair
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hair = "light"
 
     def __str__(self):
         """Lannister str method"""
@@ -40,10 +39,11 @@ class Lannister(Character):
     def __repr__(self):
         """Lannister repr method"""
         return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hair}')"
-    
+
     @classmethod
-    def create_lannister(first_name, is_alive=True):
-        return self.__init__()
+    def create_lannister(cls, first_name, is_alive=True):
+        """Lannister create_lannister method"""
+        return Lannister(first_name, is_alive)
 
     def die(self):
         """Lannister die method"""
