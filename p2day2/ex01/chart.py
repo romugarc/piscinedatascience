bc81e6ee562623452818c03fe7048093b55ff193
Romuald Garcia <rgarcia@z3r3p5.42lyon.fr>
2025-03-13 19:35:41 +0100

    chart1 done

--- p2day2/ex01/chart.py
+++ p2day2/ex01/chart.py
@@ -3,7 +3,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
 from dotenv import load_dotenv
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import pandas as pd
+
+def line_graph(timedf):
+    """
+    Creates a line graph that displays the number of customers every day from Oct 2022 to Jan 2023
+    """
+    newdf = timedf.value_counts('date')
+    newdf = newdf.sort_index()
+    plt.ylabel("Number of customers")
+    plt.plot(newdf)
+    ax = plt.gca()
+    ax.set_facecolor('#EAEAF2')
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+    plt.xlim(newdf.index[0], newdf.index[-1])
+    plt.grid(color='w')
+    plt.show()
 
 
 def main():
@@ -20,18 +36,9 @@ def main():
     }
     try:
         engine = create_engine(f"postgresql+psycopg2://{params['user']}:{params['password']}@{params['host']}:{params['port']}/{params['dbname']}")
-        #timedf = pd.read_sql_query("""SELECT CAST(event_time as date), user_id FROM customers WHERE event_type = 'purchase'""", engine)
         timedf = pd.read_sql_query("""SELECT date(event_time),  COUNT(*) OVER (PARTITION BY date(event_time)) FROM customers WHERE event_type = 'purchase' GROUP BY event_time""", engine)
-        print(timedf)
-        newdf = timedf.value_counts('date')
-        print(newdf)
-        newdf = newdf.sort_index()
-        plt.ylabel("Number of customers")
-        plt.plot(newdf)
-        ax = plt.gca()
-        ax.set_facecolor('#EAEAF2')
-        plt.grid(color='w')
-        plt.show()
+        line_graph(timedf)
+
 
     except Exception as e:
         print(f"Error:{str(e)}")
